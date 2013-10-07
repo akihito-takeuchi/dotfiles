fdc5cdcb72287c657e30db29f5a8bcc840bad9ab
Akihito Takeuchi <atakeuti2@gmail.com>
2013-10-08 02:11:51 +0900

    Added snippet setups

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -89,6 +89,7 @@
 (setq iswitchb-regexp nil)
 (setq iswitchb-prompt-newbuffer nil)
 
+;; (install-elisp-from-emacswiki "recentf-ext.el")
 (setq recentf-max-saved-items 500)
 (setq recentf-exclude '("/TAGS$" "/var/tmp/"))
 (require 'recentf-ext)
@@ -358,6 +359,37 @@
               (define-key w3m-mode-map (kbd "<C-up>") 'w3m-previous-anchor)
               (define-key w3m-mode-map (kbd "<C-left>") 'w3m-view-previous-page)
               (define-key w3m-mode-map (kbd "<C-right>") 'w3m-view-this-url))))
+
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"
+        "~/.emacs.d/public_repos/yasnippet/snippets"))
+(yas-global-mode 1)
+(custom-set-variables '(yas-trigger-key "TAB"))
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+
+(eval-after-load "helm-config"
+  '(progn
+     (defun my-yas/prompt (prompt choices &optional display-fn)
+       (let* ((names (loop for choice in choices
+                           collect (or (and display-fn (funcall display-fn choice))
+                                       choice)))
+              (selected (helm-other-buffer
+                         `(((name . ,(format "%s" prompt))
+                            (candidates . names)
+                            (action . (("Insert snippet" . (lambda (arg) arg))))))
+                         "*helm yas/prompt*")))
+         (if selected
+             (let ((n (position selected names :test 'equal)))
+               (nth n choices))
+           (signal 'quit "user quit!"))))
+     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+     (define-key helm-command-map (kbd "y") 'yas/insert-snippet)))
+
+;; snippet-mode for *.yasnippet files
+(add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
 
 ;------------------------
 ; Setup for python mode
