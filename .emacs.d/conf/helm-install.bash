a233607f4e33e8c542e0c9bd96b47007c35a57e3
Akihito Takeuchi <atakeuti@gmail.com>
2015-03-21 02:31:48 +0900

    Updated the helm install script

--- .emacs.d/conf/helm-install.bash
+++ .emacs.d/conf/helm-install.bash
@@ -3,5 +3,8 @@
 public_dir=`dirname $0`/../public_repos
 cd $public_dir
 git clone https://github.com/emacs-helm/helm
+git clone https://github.com/jwiegley/emacs-async
 cd helm
-git checkout v1.4.9
+git checkout v1.6.8
+make
+
